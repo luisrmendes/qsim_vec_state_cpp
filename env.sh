6a6cd278616d7b1ec8ebbb3556e515850f16a6c1
luisrmendes <lrmmendes98@gmail.com>
2026-01-21 10:31:00 +0000

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,15 +1,16 @@
 # qsim_vec_state_cpp/env.sh
 
-# Only set aliases if inside this project
-PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [[ "$PWD" == "$PROJECT_ROOT"* ]]; then
+# Build
+alias b="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug && cmake --build build"
+# Iterative build and run
+alias r='b && ./build/my_program'
+# Build Release
+alias br="cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
+# Clean Build Artifacts
+alias cln="rm -rf build coverage coverage.info"
+# Run UTs
+alias tst="ctest --test-dir build --verbose"
+# Run coverage
+alias tstcv="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON && cmake --build build && ctest --test-dir build && lcov -c -d build -o coverage.info --ignore-errors unused,mismatch && lcov --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
 
-    alias b="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug && cmake --build build"
-    alias br="cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
-    alias r='b && ./build/my_program'
-    alias cln="rm -rf build coverage coverage.info"
-    alias tst="ctest --test-dir build --verbose"
-    alias tstcv="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON && cmake --build build && ctest --test-dir build && lcov -c -d build -o coverage.info --ignore-errors unused,mismatch && lcov --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
-
-    echo "Project aliases loaded: r, b, br, cln, tst, tstcv"
-fi
+echo "Project aliases loaded: r, b, br, cln, tst, tstcv"

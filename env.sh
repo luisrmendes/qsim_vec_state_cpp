1d3f514d86a282d6b889d39aed750c6312892d8a
luisrmendes <lrmmendes98@gmail.com>
2026-01-21 09:17:06 +0000

    Update env.sh

--- env.sh
+++ env.sh
@@ -7,9 +7,9 @@ if [[ "$PWD" == "$PROJECT_ROOT"* ]]; then
     alias b="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug && cmake --build build"
     alias br="cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
     alias r='b && ./build/my_program'
-    alias cln='alias clean="rm -rf build coverage coverage.info"'
+    alias cln="rm -rf build coverage coverage.info"
     alias tst="ctest --test-dir build --verbose"
-    alias tstcv="b && lcov --ignore-errors unused --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
+    alias tstcv="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON && cmake --build build && ctest --test-dir build && lcov -c -d build -o coverage.info --ignore-errors unused,mismatch && lcov --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
 
     echo "Project aliases loaded: r, b, br, cln, tst, tstcv"
 fi

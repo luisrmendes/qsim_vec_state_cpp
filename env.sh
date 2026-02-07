3b31e280d2a2d1d1d2da45cc82217c1c43dc1ba2
luisrmendes <lrmmendes98@gmail.com>
2026-02-07 09:12:13 +0000

    Update to C++23 version, update env.s

--- env.sh
+++ env.sh
@@ -9,8 +9,8 @@ alias br="cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build 
 # Clean Build Artifacts
 alias cln="rm -rf build coverage coverage.info"
 # Run UTs
-alias tst="ctest --test-dir build --verbose"
+alias tst="b && ctest --test-dir build --verbose"
 # Run coverage
-alias tstcv="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON && cmake --build build && ctest --test-dir build && lcov -c -d build -o coverage.info --ignore-errors unused,mismatch && lcov --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
+alias tstcv="b && cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON && cmake --build build && ctest --test-dir build && lcov -c -d build -o coverage.info --ignore-errors unused,mismatch && lcov --remove coverage.info '/usr/*' '*/tests/*' '*_deps/googletest-src/*' --output-file coverage.info && genhtml coverage.info --output-directory coverage"
 
 echo "Project aliases loaded: r, b, br, cln, tst, tstcv"
